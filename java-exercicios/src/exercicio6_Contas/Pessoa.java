0d474b14994fa3c08a30e9f8aa9b5ed216544583
Batatexy <bielcarrascosa@gmail.com>
2025-03-18 14:46:05 -0300

    Feat: Exercicio de Cadastro de Pessoas e Contas Concluido, com uma versão mais simples, e outra com um menu de navegação dinamico, criação de pessoas, contas, dando para as contas fazerem transferencias entre elas

---
+++ java-exercicios/src/exercicio6_Contas/Pessoa.java
@@ -0,0 +1,49 @@
+package exercicio6_Contas;
+
+import java.util.ArrayList;
+
+public class Pessoa
+{
+	private String nome;
+	private String cpf;
+	private Conta conta;
+
+	public static ArrayList<Pessoa> pessoas = new ArrayList<>();
+
+	public Pessoa(String nome, String cpf)
+	{
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	public void criarConta(int agencia, String senha)
+	{
+		conta = new Conta(agencia, senha);
+	}
+
+	public void imprime()
+	{
+		System.out.println("Nome: " + this.nome);
+		System.out.println("CPF: " + this.cpf);
+
+		if (conta != null)
+		{
+			this.conta.imprime();
+		} else
+		{
+			System.out.println("Ainda não possui uma Conta\n");
+		}
+
+	}
+
+	public Conta getConta()
+	{
+		return this.conta;
+	}
+
+	public String getNome()
+	{
+		return this.nome;
+	}
+
+}
